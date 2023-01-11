afb992f80ccd4fae36c8d3e472d241aceab7ba25
Nicolas VELATI <nicolas.velati@sii.fr>
2023-01-11 22:11:38 +0100

    feat: added Unlockle, Waffle, Wordle

---
+++ api/src/challenge/parsers/wordle.parser.ts
@@ -0,0 +1,34 @@
+import { Injectable } from "@nestjs/common";
+import { TurnResultEnum } from "../turn-result.enum";
+import { TurnParser } from "./turn-parser.interface";
+
+@Injectable()
+export class WordleParser extends TurnParser {
+  getChallengeName(): string {
+    return "Wordle";
+  }
+
+  handles(rawResult: string): boolean {
+    return (
+      this.getLine(rawResult, 1).match(/Wordle [0-9]+/) !== null
+    );
+  }
+
+  extractGameNumber(rawResult: string): number {
+    return +this.extractData(this.getLine(rawResult, 1), /[0-9]+/);
+  }
+
+  extractScore(rawResult: string): string {
+    try {
+      return this.extractData(this.getLine(rawResult, 1), /[0-6]+\/[0-6]+/);
+    } catch {
+      return "";
+    }
+  }
+
+  extractResult(rawResult: string): TurnResultEnum {
+    return this.extractScore(rawResult)
+      ? TurnResultEnum.WON
+      : TurnResultEnum.LOST;
+  }
+}
