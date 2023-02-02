137df10721ea7a401635589c15fb1de909925044
Nicolas VELATI <nicolas.velati@sii.fr>
2023-02-02 23:24:12 +0100

    feat(api): added Nerdle

---
+++ api/src/challenge/parsers/nerdle.parser.ts
@@ -0,0 +1,26 @@
+import { Injectable } from "@nestjs/common";
+import { TurnResultEnum } from "../enums/turn-result.enum";
+import { TurnParser } from "./turn-parser.interface";
+
+@Injectable()
+export class NerdleParser extends TurnParser {
+  getChallengeName(): string {
+    return "Nerdle";
+  }
+
+  handles(rawResult: string): boolean {
+    return this.getLine(rawResult, 1).match(/nerdlegame [0-9]+/) !== null;
+  }
+
+  extractGameNumber(rawResult: string): number {
+    return +this.extractData(this.getLine(rawResult, 1), /[0-9]+/);
+  }
+
+  extractScore(rawResult: string): string {
+    return this.extractData(this.getLine(rawResult, 1), /[0-6]+\/[0-6]+/);
+  }
+
+  extractResult(rawResult: string): TurnResultEnum {
+    return TurnResultEnum.WON;
+  }
+}
